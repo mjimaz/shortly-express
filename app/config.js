20d378804eb7c09e000c9d2887492211c68b617c
mjimaz <m.aljimaz@gmail.com>
2016-02-08 19:04:25 -0800

    Implement USERS schema and implement Users Bookshelf model

--- app/config.js
+++ app/config.js
@@ -35,9 +35,18 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
   }
 });
 
-/************************************************************/
-// Add additional schema definitions below
-/************************************************************/
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 25);
+      user.string('password', 25);
+      user.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
 
 module.exports = db;
