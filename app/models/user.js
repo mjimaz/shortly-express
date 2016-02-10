1bce03cd523a37c2e50fc4e083b82cc7653e05bd
mjimaz <m.aljimaz@gmail.com>
2016-02-09 19:26:59 -0800

    Remove custom fetch function

--- app/models/user.js
+++ app/models/user.js
@@ -6,18 +6,6 @@ var User = db.Model.extend({
   tableName: 'users',
   hasTimestamps: true,
   
-  // Built in Function
-  // fetch: function () {
-  //   return db.knex.select().from('users')
-  //     .where('username', this.get('username'))
-  //     .then(function (rows) {
-  //       return rows[0];
-  //     })
-  //     .catch(function (error) {
-  //       console.error('user not found', error);
-  //     });
-  // },
-
   createUser: function () {
     var salt = bcrypt.genSaltSync(10);
     var hash = bcrypt.hashSync(this.get('password'), salt);
